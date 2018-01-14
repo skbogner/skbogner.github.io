29efec217dcec8108ab3d399fa640106a2e30bea
Sebastian Bogner <bogner.sebastian@gmail.com>
2018-01-14 15:29:13 +0100

    Added semantic ui

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import {
+  Button
+} from 'semantic-ui-react'
 
 export default function Home () {
   return (
@@ -16,9 +19,9 @@ export default function Home () {
         Please see the for instructions on how to
         use this boilerplate to deploy your own single page app using GitHub Pages.
       </p>
-      <div>
-        <Link to='/example'>Example page</Link>
-      </div>
+      <Link to='/example'>
+        <Button>Example page</Button>
+      </Link>
       <div>
         <Link to='/example/two-deep?field1=foo&field2=bar#boom!'>
           Example two deep with query and hash
